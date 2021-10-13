c70a51098952a2af5bc366b5da8b988e57ba9d07
dabump <martin@martincoetzee.com>
2021-10-14 09:42:06 +1100

    initial concept

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dabump/token-bucket
+
+go 1.17
---
+++ internal/token/bucket.go
@@ -0,0 +1,48 @@
+package token
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const hitCount = 1
+
+type Bucket struct {
+	rate         int16
+	designation  string
+	rateDuration time.Duration
+
+	mutex sync.Mutex
+	availableTokens int16
+}
+
+func NewBucket(designation string, rate int16, duration time.Duration) *Bucket {
+	b := Bucket{
+		rate: rate,
+		rateDuration: duration,
+		designation: designation,
+	}
+	b.availableTokens = rate
+	return &b
+}
+
+func (b *Bucket) hit() bool {
+	b.mutex.Lock()
+	if b.availableTokens > 0 || (b.availableTokens - hitCount) > 0 {
+		b.availableTokens -= hitCount
+		fmt.Printf("reducing token count by %d, availble tokens %d\n", hitCount, b.availableTokens)
+		b.mutex.Unlock()
+		return true
+	}
+	b.mutex.Unlock()
+	fmt.Print("insufficient tokens available\n")
+	return false
+}
+
+func (b *Bucket) fill()  {
+	b.mutex.Lock()
+	fmt.Printf("available tokens %d, refilling back to %d\n", b.availableTokens, b.rate)
+	b.availableTokens = b.rate
+	b.mutex.Unlock()
+}
